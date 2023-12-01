28e1ef4f26cd25db6e995ff323db30d4c7f12fdc
Caue Peruque <cauepptrybe@gmail.com>
2023-12-01 16:57:31 -0300

    feat: req09

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -12,8 +12,8 @@ public class TrybeGamesDatabase
     public List<Game> GetGamesDevelopedBy(GameStudio gameStudio)
     {
         var filteredGamesByDeveloper = from game in Games
-                                        where game.DeveloperStudio == gameStudio.Id
-                                        select game;
+                                       where game.DeveloperStudio == gameStudio.Id
+                                       select game;
         List<Game> games = filteredGamesByDeveloper.ToList();
 
         return games;
@@ -26,7 +26,7 @@ public class TrybeGamesDatabase
                                     from gameOwned in player.GamesOwned
                                     where game.Id == gameOwned
                                     select game;
-        
+
         List<Game> games = filteredGamesByPlayer.ToList();
         return games;
     }
@@ -35,10 +35,10 @@ public class TrybeGamesDatabase
     public List<Game> GetGamesOwnedBy(Player playerEntry)
     {
         var filteredGamesOwnedByPlayer = from game in Games
-                                    from gameOwned in playerEntry.GamesOwned
-                                    where game.Id == gameOwned
-                                    select game;
-        
+                                         from gameOwned in playerEntry.GamesOwned
+                                         where game.Id == gameOwned
+                                         select game;
+
         List<Game> games = filteredGamesOwnedByPlayer.ToList();
         return games;
     }
@@ -48,35 +48,60 @@ public class TrybeGamesDatabase
     public List<GameWithStudio> GetGamesWithStudio()
     {
         var filteredGamesByDeveloper = from game in Games
-                                        from gameDevelopedBy in GameStudios
-                                        where game.DeveloperStudio == gameDevelopedBy.Id
-                                        select new GameWithStudio { GameName = game.Name, StudioName = gameDevelopedBy.Name, NumberOfPlayers = game.Players.Count() };  
-        List<GameWithStudio> games = filteredGamesByDeveloper.ToList();   
-        
-        return games;                 
+                                       from gameDevelopedBy in GameStudios
+                                       where game.DeveloperStudio == gameDevelopedBy.Id
+                                       select new GameWithStudio { GameName = game.Name, StudioName = gameDevelopedBy.Name, NumberOfPlayers = game.Players.Count() };
+        List<GameWithStudio> games = filteredGamesByDeveloper.ToList();
+
+        return games;
     }
-    
+
     // 8. Crie a funcionalidade de buscar todos os diferentes Tipos de jogos dentre os jogos cadastrados
     public List<GameType> GetGameTypes()
     {
         var queryGamesTypes = from game in Games
-                            group game by game.GameType into games
-                            select games;
-        
+                              group game by game.GameType into games
+                              select games;
+
         List<GameType> gamesTypes = new List<GameType>();
-        foreach(var game in queryGamesTypes)
+        foreach (var game in queryGamesTypes)
         {
             gamesTypes.Add(game.Key);
         }
 
-        return gamesTypes;  
+        return gamesTypes;
     }
 
     // 9. Crie a funcionalidade de buscar todos os estúdios de jogos junto dos seus jogos desenvolvidos com suas pessoas jogadoras
     public List<StudioGamesPlayers> GetStudiosWithGamesAndPlayers()
     {
-        // Implementar
-        throw new NotImplementedException();
+        var studios = GameStudios.ToList();
+        var results = new List<StudioGamesPlayers>();
+
+        foreach (var studio in studios)
+        {
+            var games = Games.Where(game => game.DeveloperStudio == studio.Id).ToList();
+            var gamePlayers = new List<GamePlayer>();
+
+            foreach (var game in games)
+            {
+                var players = Players.Where(player => player.GamesOwned.Contains(game.Id)).ToList();
+                gamePlayers.Add(new GamePlayer
+                {
+                    GameName = game.Name,
+                    Players = players
+                });
+            }
+
+            var studioGamesPlayers = new StudioGamesPlayers
+            {
+                GameStudioName = studio.Name,
+                Games = gamePlayers
+            };
+
+            results.Add(studioGamesPlayers);
+        }
+        return results;
     }
 
 }
